5da6ae7c3299717c6c93730e936a5d7d6bb0246f
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-01-02 17:20:47 +0100

    Refactor coded number tests

--- test/test_coded_number.py
+++ test/test_coded_number.py
@@ -1,4 +1,19 @@
+import pytest
 from flac.coded_number import encode, decode
+
+
+# [(decoded, encoded, size)]
+TESTS = [
+    # Single byte values
+    (0b00000000, 0b00000000, 1),
+    (0b00011111, 0b00011111, 1),
+    (0b01111111, 0b01111111, 1),
+
+    # Two bytes values (values between 8 and 11 bits)
+    (0b10000000, 0b11000010_10000000, 2),
+    (0b10101010_1, 0b11000101_10010101, 2),
+    (0b10101010_101, 0b11010101_10010101, 2)
+]
 
 
 def encode_(x: int):
@@ -9,25 +24,7 @@ def decode_(x: int, n: int):
     return decode(x.to_bytes(n, byteorder='big'))
 
 
-def test_encode():
-    # Single byte values
-    assert encode_(0b00000000) == 0b00000000
-    assert encode_(0b00011111) == 0b00011111
-    assert encode_(0b01111111) == 0b01111111
-
-    # Two bytes values (values between 8 and 11 bits)
-    assert encode_(0b10000000) == 0b11000010_10000000
-    assert encode_(0b10101010_1) == 0b11000101_10010101
-    assert encode_(0b10101010_101) == 0b11010101_10010101
-
-
-def test_decode():
-    # Single byte values
-    assert decode_(0b00000000, 1) == 0b00000000
-    assert decode_(0b00011111, 1) == 0b00011111
-    assert decode_(0b01111111, 1) == 0b01111111
-
-    # Two bytes values (values between 8 and 11 bits)
-    assert decode_(0b11000010_10000000, 2) == 0b10000000
-    assert decode_(0b11000101_10010101, 2) == 0b10101010_1
-    assert decode_(0b11010101_10010101, 2) == 0b10101010_101
+@pytest.mark.parametrize(("decoded", "encoded", "size"), TESTS)
+def test(decoded, encoded, size):
+    assert encode_(decoded) == encoded
+    assert decode_(encoded, size) == decoded
